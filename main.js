9a78ae2530353735858020eb44daf692d2bafc66
DoneRaging <billing.caleb14@gmail.com>
2019-07-29 13:16:23 +0930

    Comments

--- main.js
+++ main.js
@@ -7,9 +7,12 @@ const {app , BrowserWindow, Menu, remote, ipcMain} = electron;
 let mainWindow;
 
 app.on('ready', function() {
+    // Create a new Electron window
     mainWindow = new BrowserWindow({
         title: 'Round Robin Chat'
     });
+
+    // Load HTML into the Electron window
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'mainWindow.html'),
         protocol: 'file',
